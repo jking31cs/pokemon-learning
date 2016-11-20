e42249b75b475e2a5041afe64a7de20b403570d5
jking31 <james.kingiii@gtri.gatech.edu>
2016-11-20 15:29:16 -0500

    skeleton code for pokemon analysis.

---
+++ src/main/java/com/jking31cs/PokemonWinAnalysis.java
@@ -0,0 +1,38 @@
+package com.jking31cs;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.jking31cs.state.BattleTree;
+
+/**
+ * Created by jking31 on 11/20/16.
+ */
+public class PokemonWinAnalysis {
+
+    private double wins = 0;
+    private double battles = 0;
+    private final String pokemon;
+
+    public PokemonWinAnalysis(String pokemon) {
+        this.pokemon = pokemon;
+    }
+
+    public Double getConfidence() throws IOException {
+        //TODO all your team gathering and stuff here.
+        List<String> teamIds = TeamReader.pokemonTeamIndex().get(pokemon);
+        for (String teamId : teamIds) {
+            Map<String, BattleTree> battles = ReadInDataObj.battlesForTeam(teamId);
+            for (BattleTree bt : battles.values()) {
+                analyzeBattle(bt);
+            }
+        }
+        return wins/battles;
+    }
+
+    private void analyzeBattle(BattleTree tree) {
+        //TODO incremenet win and battle bassed on tree data.
+    }
+
+}
