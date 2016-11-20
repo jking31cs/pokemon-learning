b04a714005085f7997771b2cd6fdc79c0df1d94f
jking31 <james.kingiii@gtri.gatech.edu>
2016-11-20 15:02:26 -0500

    Created Battle tree fetching for given team id.

--- src/main/java/com/jking31cs/ReadInDataObj.java
+++ src/main/java/com/jking31cs/ReadInDataObj.java
@@ -3,6 +3,7 @@ package com.jking31cs;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.JavaType;
@@ -29,5 +30,16 @@ public class ReadInDataObj {
             Map<String, BattleTree> readInValues = om.readValue(battleFile, mapType);
             System.out.println(readInValues);
         }
+    }
+
+    public static Map<String, BattleTree> battlesForTeam(String id) throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new GuavaModule());
+        JavaType mapType = om.getTypeFactory().constructMapType(HashMap.class, String.class, BattleTree.class);
+        String dirKey = id.substring(0,2);
+        File battleFile = new File("output/simulated-battles/" + dirKey + "/battle-" + id + ".json");
+        if (!battleFile.exists()) throw new IllegalArgumentException("No battle trees exist for this team id.");
+        Map<String, BattleTree> readInValues = om.readValue(battleFile, mapType);
+        System.out.println(readInValues);
     }
 }
